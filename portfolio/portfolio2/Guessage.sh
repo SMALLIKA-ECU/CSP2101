45fd2af75f8cf66d47aec0e205c4a40cf7171574
Sachini Mallikarachchi <SMALLIKA-ECU@github.com>
2020-01-26 10:57:23 +0000

    Initial project added

--- portfolio/portfolio2/Guessage.sh
+++ portfolio/portfolio2/Guessage.sh
@@ -1,21 +1,28 @@
 #!/bin/bash
 #Author: Sachini Mallikarachchi (10518627)
-#Task 1 - Script to Guess Age
+#Task 1 -Script to Guess Age
 
-#Prompt the user to guess an age between 20 and 70
+#Function to generate a random age between 20 and 70
+Generate_random()
+{
+    (( answer = 20 + RANDOM % 70 ))
+}
+
+#Function to play the game
+Play_game()
+{
+    while (( guess != answer )); do
+        read -p "Enter guess: " guess
+        if (( guess < answer )); then
+            echo "Too Low"
+        elif (( guess > answer )); then
+            echo "Too High" 
+        fi 
+    done
+    echo -e "Your guess is correct!"
+}
+
+Generate_random
+#Prompt user to guess age between 20 and 70
 echo -e "Guess an age number between 20 and 70\n"
-
-#Genete random age 
-(( answer = 20 + RANDOM % 70 ))
-
-#Play the game
-while (( guess != answer )); do
-    read -p "Enter guess: " guess
-    if (( guess < answer )); then
-        echo "Too Low"
-    elif (( guess > answer )); then 
-        echo "Too High" 
-    fi
-done
-echo -e "Your guess is correct!" 
-
+Play_game
