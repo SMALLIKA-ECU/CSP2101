c1c7191a2354144f8fc95b7c8e07bda077e29afa
Sachini Mallikarachchi <SMALLIKA-ECU@github.com>
2020-01-26 11:43:46 +0000

    Initial project added

--- portfolio/portfolio2/Rectangle.sh
+++ portfolio/portfolio2/Rectangle.sh
@@ -4,6 +4,6 @@
 #This script will obtain the dimensions of rectangles from the rectangle.txt file
 
 awk 'BEGIN {FS=","}
-    { print "Name: ", $1, "Height: ", $2, "Width: ", $3, "Area: ", $4, "Colour: ", $5 }
+    { print "Name: ", $1, ":", "Height: ", $2, ":", "Width: ", $3, ":", "Area: ", $4, ":", "Colour: ", $5 }
     END { print "Done" }' ./rectangle.txt
     
